8a7e6f369a4f0b1d9269702251acaac71cf6da68
tersoo <latifwork14@gmail.com>
2024-12-19 18:29:26 +0300

    Article Response tasarımı tamamlandı

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Article() {
     return (
-        <div className="grid grid-cols-3 mt-20 gap-x-7 pb-10">
+        <div className="grid grid-cols-1 md:grid-cols-3 place-items-center px-4 md:px-0 mt-20 gap-y-5 md:gap-x-7 pb-10">
             <div>
                 <img
                     src="https://cdn.dsmcdn.com/ty1612/pimWidgetApi/mobile_20241212090954_3208386KadinMobile202412121102.jpg"
